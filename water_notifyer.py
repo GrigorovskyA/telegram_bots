33cb33dbaf23eeb2e1b4137eeb312452867c62c0
Alexey <grigorovsky.alexey@gmail.com>
2019-11-20 15:39:52 +0300

    refactor

--- water_notifyer.py
+++ water_notifyer.py
@@ -2,11 +2,8 @@ import telegram, smtplib
 
 from datetime import datetime
 from telegram.ext import Updater, CommandHandler
+from variables import *
 
-token = '794614573:AAHSWRgPRwHYGwI6L4kYo6S7g3rOdfbuKNw'
-our_code = '3366572'
-gmail_user = 'grigorovsky.alexey@gmail.com'
-gmail_app_password = 'PASS_SHOULD_BE_THERE'
 bot = telegram.Bot(token)
 
 def start(update, context):
@@ -14,7 +11,8 @@ def start(update, context):
 
 def vasilich(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Время сдавать показатели воды?\n'
-                                                                    'Пришли показатели через /vodichka значение холодной '
+                                                                    'Пришли показатели через /vodichka '
+                                                                    'проверочный код, значение холодной '
                                                                     'и горячей воды через пробелы.')
 
 def vodichka(update, context):
@@ -29,45 +27,46 @@ def vodichka(update, context):
 
         if secure_code == our_code:
             try:
-                sent_to = ['elminster@mail.ru','katehollywood@mail.ru']
                 context.bot.send_message(chat_id=update.effective_chat.id,
                                          text='Письмо было отослано на ' +
                                               ', '.join(sent_to) + ' ящики от ' + datetime.now().strftime("%m/%d/%Y"))
 
                 sent_from = gmail_user
                 sent_subject = "Показания воды за" + datetime.now().strftime("%m/%d/%Y")
-                sent_body = ("Привет, Пишет тебе бот\n"
+                email_body = ("Привет, Пишет тебе бот\n"
                              "Показания холодной воды: %s\n"
                              "Показания холодной воды: %s\n" % (cold_water, hot_water)
                              )
 
-                email_text = """\
-                From: %s
-                To: %s
-                Subject: %s
-                
-                %s
-                """ % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+                sent_body = '\r\n'.join(['To: %s' % sent_to,
+                                    'From: %s' % sent_from,
+                                    'Subject: %s' % sent_subject,
+                                    '',
+                                    email_body])
 
                 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
                 server.ehlo()
                 server.login(gmail_user, gmail_app_password)
-                server.sendmail(sent_from, sent_to, email_text)
+                server.sendmail(sent_from, sent_to, sent_body.encode('utf-8'))
                 server.close()
             except Exception as exception:
                 context.bot.send_message(chat_id=update.effective_chat.id,
                                          text="Error: %s!\n\n" % exception)
         else:
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text='Введен неправильный код безопасности!\nПисьмо не было отослано.')
+                                     text='Введен неправильный код безопасности!\n'
+                                          'Письмо не было отослано.\nГуляй отсюда петушок')
     else:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='Ты должен передать 3 числа:\n'
+                                 text='Ты должен передать /vodichka и 3 числа:\n'
                                       '1. Проверочный код\n'
                                       '2. Холодная вода\n'
                                       '3. Горячая вода\n')
 
-updater = Updater(token, use_context=True)
+if proxy_kwargs:
+    updater = Updater(token, use_context=True, request_kwargs=proxy_kwargs)
+else:
+    updater = Updater(token, use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('vasilich', vasilich))
 updater.dispatcher.add_handler(CommandHandler('vodichka', vodichka))
